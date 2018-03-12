87320aefac6a2a9ec46564eddf9c255219581479
Paco Lee <paco.lee@twotalltotems.com>
2018-03-12 15:04:01 -0700

    Added Node.js

---
+++ script.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'sampleDB'
+});
+
+connection.connect(function() {
+	if(!!error) {
+		console.log('Error');
+	} else {
+		console.log('Connected');
+	}
+})
+
+app.get('/', function(req, resp) {
+	connection.query("SELECT * FROM book", function(error, rows, fields){
+		if(!!error) {
+			console.log('Error in the query');
+		} else {
+			console.log('SUCCESs!\n')
+			console.log(rows);
+		}
+	})
+})
+
+app.listen(1337);
