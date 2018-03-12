e2be64e606512008837245e42678636d742d78bc
nkevin358 <34780481+nkevin358@users.noreply.github.com>
2018-03-12 00:45:56 -0700

    Added "genre" attribute to table

--- SQL Table Creations/Book.sql
+++ SQL Table Creations/Book.sql
@@ -3,6 +3,7 @@ CREATE TABLE Book
 	 title		varchar(100),
 	 author 	varchar(100),
 	 publisher	varchar(100),
+	 genre		varchar(100),
  PRIMARY KEY (isbn),
  UNIQUE (title, author));
 
--- SQL Table Creations/LibraryBook.sql
+++ SQL Table Creations/LibraryBook.sql
@@ -2,6 +2,7 @@ CREATE TABLE LibraryBook(bookID varchar(100),
 isbn 		varchar(13) NOT NULL,
 branchNum 	varchar(100) NOT NULL,
 status		varchar(100) NOT NULL,
+genre		varchar(100),
 PRIMARY KEY (bookID), 
 FOREIGN KEY (isbn) REFERENCES Book(isbn)
 ON UPDATE CASCADE
