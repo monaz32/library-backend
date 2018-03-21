af040752d7212ba819b1a89d802a167d71e06f06
Nancy Wu <nancy.wu@coe.ubc.ca>
2018-03-20 18:18:18 -0700

    added some eployee api calls

--- routes/employee.js
+++ routes/employee.js
@@ -1,22 +1,65 @@
 //routes
-module.exports = function(app) {  
+module.exports = function(app) {
     app.route('/employee')
-        .get(getAPI)
-        .post(postAPI)
-        .put(putAPI);
+        .get(getEmployee)
+        .post(addEmployee)
+        .put(updateEmployee)
+        .delete(deleteEmployee);
+
+    app.route('/employee/:id')
+        .get(getEmployee);
+
+    app.route('/employee/:id')
+        .post(addEmployee);
+
+    app.route('/employee/:id')
+        .put(updateEmployee);
+
+    app.route('/employee/:id')
+        .delete(deleteEmployee);
+
+    app.route('employee/:id/:name')
+
+
 }
 
 var connection = require('../server').connection;
 
 //API functions
-function getAPI(request, response) {
+function getEmployee(request, response) {
+    connection.connect(function(error) {
+        if(!!error) {
+            console.log('Error\n');
+            throw error;
+        }
+        console.log('Connected\n');
 
+        connection.query('SELECT * FROM Employee', function(error, rows, fields){
+            if(!!error) {
+                console.log('Error in the query\n');
+                throw error;
+            }
+
+            console.log('query SUCCESS!\n')
+            response.send(rows);
+            connection.end();
+        });
+    });
 }
 
-function postAPI(request, response) {
-
+//API functions
+function getEmployee(request, response) {
+    response.send(request.params.id);
 }
 
-function putAPI(request, response) {
-	
+function updateEmployee(request, response) {
+    response.send(request.body.employee);
+}
+
+function addEmployee(request, response) {
+    response.send(request.body.employee);
+}
+
+function deleteEmployee(request, response) {
+    response.send(request.body.employee);
 }
