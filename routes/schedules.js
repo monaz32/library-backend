e2f612b93b8bbcb4c6efb6d63c718bdbfd048609
Mona Zhao <monaz32@hotmail.com>
2018-03-21 16:06:30 -0700

    addSchedules phase 1 #9

--- routes/schedules.js
+++ routes/schedules.js
@@ -29,7 +29,40 @@ function getSchedules(request, response) {
 }
 
 function addSchedule(request, response) {
-  //response.send(request.body.book);
+  connection.connect(function(error) {
+    console.log('Connected to database\n');
+
+    var fromTime;
+    var fromDate;
+    var toTime;
+    var toDate;
+
+    var insertTimePeriod = 'INSERT INTO TimePeriod value(\'' + fromTime + '\', \'' +
+      toTime + '\', \'' + fromDate + '\', \'' + toDate + '\')';
+
+    connection.query(insertTimePeriod, function(error, rows, fields){
+      if(!!error) {
+        console.log('Error in addSchedule query: failed to add to TimePeriod\n');
+        throw error;
+      }
+      console.log('addSchedule query SUCCESS: added to TimePeriod\n')
+
+      var id; // should be num
+      var roomName;
+
+      var insertSchedule = 'INSERT INTO schedules value(' + id + ', \'' + roomName + '\', \'' +
+        fromTime + '\', \'' + toTime + '\', \'' + fromDate + '\', \'' + toDate + '\')';
+
+      connection.query(insertSchedule, function(error, row, fields) {
+        if(!!error) {
+          console.log('Error in addSchedule query: failed to add to Schedules\n');
+          throw error;
+        }
+
+        console.log('addSchedule query SUCCESS: added to Schedules\n')
+      })
+    });
+  });
 }
 
 function updateSchedule(request, response) {
