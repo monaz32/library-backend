0a67dc251c1f15976046f9699a20e61f75d6261a
Nancy Wu <nancy.wu@coe.ubc.ca>
2018-03-22 22:57:11 -0700

    Issue #3 getEvents

--- routes/event.js
+++ routes/event.js
@@ -1,17 +1,46 @@
 //routes
 module.exports = function(app) {  
     app.route('/event')
-        .get(getAPI)
-        .post(postAPI)
-        .put(putAPI);
+        .get(getEvents)
+        .post(addEvent)
+        .put(updateEvent);
+
+    app.route('/event/id/:id')
+        .get(getEventId)
+        .delete(deleteEvent);
+
+    app.route('/event/location/:location')
+        .get(getEventbyLocation);
+
+
 }
 
 var connection = require('../server').connection;
 
 //API functions
-function getAPI(request, response) {
+function getEvents(request, response) {
+    connection.connect(function(error) {
+        if(!!error) {
+            console.log('Error\n');
+            throw error;
+        }
+        console.log('Connected\n');
 
+        var query =''
+
+        connection.query('SELECT * FROM ev', function(error, rows, fields){
+            if(!!error) {
+                console.log('Error in the query\n');
+                throw error;
+            }
+
+            console.log('query SUCCESS!\n')
+            response.send(rows);
+            //connection.end();
+        });
+    });
 }
+
 
 function postAPI(request, response) {
 
