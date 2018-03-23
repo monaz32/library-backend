6cce1cf9d8e3330e2f6aca63743ed7e1b13fb97d
Nancy Wu <nancy.wu@coe.ubc.ca>
2018-03-22 22:55:06 -0700

    Issue#2 get,post,delete

--- routes/employee.js
+++ routes/employee.js
@@ -1,24 +1,19 @@
 //routes
+//routes
 module.exports = function(app) {
     app.route('/employee')
+        .get(getEmployees)
+        .post(addEmployee);
+
+
+    app.route('/employee/id/:id')
         .get(getEmployee)
-        .post(addEmployee)
         .put(updateEmployee)
         .delete(deleteEmployee);
 
-    app.route('/employee/:id')
-        .get(getEmployee);
+    app.route('/employee/name/:name')
+        .get(getEmployeeName);
 
-    app.route('/employee/:id')
-        .post(addEmployee);
-
-    app.route('/employee/:id')
-        .put(updateEmployee);
-
-    app.route('/employee/:id')
-        .delete(deleteEmployee);
-
-    app.route('employee/:id/:name')
 
 
 }
@@ -26,7 +21,7 @@ module.exports = function(app) {
 var connection = require('../server').connection;
 
 //API functions
-function getEmployee(request, response) {
+function getEmployees(request, response) {
     connection.connect(function(error) {
         if(!!error) {
             console.log('Error\n');
@@ -34,7 +29,7 @@ function getEmployee(request, response) {
         }
         console.log('Connected\n');
 
-        connection.query('SELECT * FROM Employee', function(error, rows, fields){
+        connection.query('SELECT * FROM employee', function(error, rows, fields){
             if(!!error) {
                 console.log('Error in the query\n');
                 throw error;
@@ -42,24 +37,93 @@ function getEmployee(request, response) {
 
             console.log('query SUCCESS!\n')
             response.send(rows);
-            connection.end();
+            //connection.end();
         });
     });
 }
 
-//API functions
-function getEmployee(request, response) {
-    response.send(request.params.id);
+function addEmployee(request, response) {
+    console.log(request.body);
+    var employ = request.body[0];
+    var email = employ.email;
+    var sin = employ.sin;
+    var name = employ.name;
+    var address = employ.address;
+    var phoneNum = employ.phoneNum;
+    var branch = employ.branch;
+    var admin = employ.admin;
+
+
+    var query = 'INSERT INTO employee(eEmail,SIN,ename,eAddress,ePhoneNumber,branchNum,admin) '+ 'VALUES' +'(' + '\'' + email + '\',' + '\'' + sin + '\',' +
+        '\'' + name + '\',' + '\'' + address + '\',' + '\'' + phoneNum + '\',' + '\'' + branch + '\'' + admin + ');'
+
+    console.log(query);
+
+    connection.query(query, function(error, rows, fields){
+        if(!!error) {
+            console.log('Error in the query\n');
+            throw error;
+        }
+
+        console.log('query SUCCESS!\n');
+        response.send(rows);
+        //connection.end();
+    });
 }
 
 function updateEmployee(request, response) {
     response.send(request.body.employee);
 }
 
-function addEmployee(request, response) {
-    response.send(request.body.employee);
+function getEmployee(request, response) {
+
+    var eid = request.params.id;
+    connection.query('SELECT * FROM employee WHERE eid=' + String(eid) +';', function(error, rows, fields){
+        if(!!error) {
+            console.log('Error in the query\n');
+            throw error;
+        }
+
+        console.log('query SUCCESS!\n');
+        response.send(rows);
+        //connection.end();
+    });
+
 }
 
-function deleteEmployee(request, response) {
-    response.send(request.body.employee);
+function getEmployeeName(request, response) {
+
+    var name = request.params.name;
+    var query = "SELECT * FROM employee WHERE ename LIKE " + "'%" + String(name) +"%';";
+    //console.log(query);
+    connection.query(query, function(error, rows, fields){
+        if(!!error) {
+            console.log('Error in the query\n');
+            throw error;
+        }
+
+        console.log('query SUCCESS!\n');
+        response.send(rows);
+        //connection.end();
+    });
+
 }
+
+
+function deleteEmployee(request, response) {
+    var eid = request.params.id;
+
+    var query = "DELETE FROM employee WHERE eid="  + eid + ";"
+    //should we do a  select * after ?
+    connection.query(query, function(error, rows, fields){
+        if(!!error) {
+            console.log('Error in the query\n');
+            throw error;
+        }
+
+        console.log('query SUCCESS!\n');
+        response.send(rows);
+        //connection.end();
+    });
+}
+
