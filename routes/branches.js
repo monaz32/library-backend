b9e4122c7f3a1cafb7b95bd6f634dca8d3942966
Mona Zhao <monaz32@hotmail.com>
2018-03-24 21:10:33 -0700

    #9: Implemented getRoomWithIDAtBranch

--- routes/branches.js
+++ routes/branches.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
   app.route('/branches/:bid/rooms')
     .get(getRoomsAtBranch);
 
-  app.route('branches/:bid/rooms/:rid')
+  app.route('/branches/:bid/rooms/:rid')
     .get(getRoomWithIDAtBranch);
 }
 
@@ -33,8 +33,10 @@ function getBranches(request, response) {
 
 /////////////////// branches/:bid ///////////////////
 
+// Returns details on a specific branch with ID bid
 function getBranchWithID(request, response) {
   var getBranchQuery = 'SELECT * FROM LibraryBranch where branchNum = ' + formatVariableForSQL(request.params.bid);
+
   connection.query(getBranchQuery, function(error, rows, fields){
     if(!!error) {
       console.log('Error in getBranchWithID query: failed to get all branches\n');
@@ -51,9 +53,11 @@ function getBranchWithID(request, response) {
 
 /////////////////// /branches/:bid/rooms ///////////////////
 
+// Returns details on all rooms at a specific branch with ID bid
 function getRoomsAtBranch(request, response) {
-  var getRoomQuery = 'SELECT * FROM Room where branchNum = ' + formatVariableForSQL(request.params.bid);
-  connection.query(getRoomQuery, function(error, rows, fields){
+  var getRoomsQuery = 'SELECT * FROM Room where branchNum = ' + formatVariableForSQL(request.params.bid);
+
+  connection.query(getRoomsQuery, function(error, rows, fields){
     if(!!error) {
       console.log('Error in getRoomsAtBranch query: failed to get all branches\n');
       logErrorToConsole(error);
@@ -68,8 +72,23 @@ function getRoomsAtBranch(request, response) {
 
 
 /////////////////// /branches/:bid/rooms ///////////////////
+
+// Returns details on the room with number rid at the branch with ID bid
 function getRoomWithIDAtBranch(request, response) {
-  response.status(404).send("needs to be implemented");
+  var getRoomQuery = 'SELECT * FROM Room where branchNum = ' + formatVariableForSQL(request.params.bid) +
+    'and roomNumber = ' + formatVariableForSQL(request.params.rid);
+
+  connection.query(getRoomQuery, function(error, rows, fields){
+    if(!!error) {
+      console.log('Error in getRoomWithIDAtBranch query: failed to get all branches\n');
+      logErrorToConsole(error);
+      response.status(422).send('422 Unprocessable Entity');
+    }
+    else {
+      console.log('getRoomWithIDAtBranch query SUCCESS\n')
+      response.send(rows);
+    }
+  });
 }
 
 
