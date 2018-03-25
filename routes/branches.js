00a834d500efc1205250afc2dbdbe7497ce486bf
Mona Zhao <monaz32@hotmail.com>
2018-03-24 21:05:48 -0700

    #9: Implemented getRoomsAtBranch

--- routes/branches.js
+++ routes/branches.js
@@ -1,14 +1,12 @@
 module.exports = function(app) {
   app.route('/branches')
-    .get(getBranches)
-    .post(addBranch);
+    .get(getBranches);
 
   app.route('/branches/:bid')
     .get(getBranchWithID);
 
   app.route('/branches/:bid/rooms')
-    .get(getRoomsAtBranch)
-    .post(addRoomToBranch);
+    .get(getRoomsAtBranch);
 
   app.route('branches/:bid/rooms/:rid')
     .get(getRoomWithIDAtBranch);
@@ -33,23 +31,18 @@ function getBranches(request, response) {
   });
 }
 
-function addBranch(request, response) {
-  response.status(404).send("needs to be implemented");
-}
-
-
 /////////////////// branches/:bid ///////////////////
 
 function getBranchWithID(request, response) {
   var getBranchQuery = 'SELECT * FROM LibraryBranch where branchNum = ' + formatVariableForSQL(request.params.bid);
   connection.query(getBranchQuery, function(error, rows, fields){
     if(!!error) {
-      console.log('Error in getBranches query: failed to get all branches\n');
+      console.log('Error in getBranchWithID query: failed to get all branches\n');
       logErrorToConsole(error);
       response.status(422).send('422 Unprocessable Entity');
     }
     else {
-      console.log('getBranches query SUCCESS\n')
+      console.log('getBranchWithID query SUCCESS\n')
       response.send(rows);
     }
   });
@@ -59,11 +52,18 @@ function getBranchWithID(request, response) {
 /////////////////// /branches/:bid/rooms ///////////////////
 
 function getRoomsAtBranch(request, response) {
-  response.status(404).send("needs to be implemented");
-}
-
-function addRoomToBranch(request, response) {
-  response.status(404).send("needs to be implemented");
+  var getRoomQuery = 'SELECT * FROM Room where branchNum = ' + formatVariableForSQL(request.params.bid);
+  connection.query(getRoomQuery, function(error, rows, fields){
+    if(!!error) {
+      console.log('Error in getRoomsAtBranch query: failed to get all branches\n');
+      logErrorToConsole(error);
+      response.status(422).send('422 Unprocessable Entity');
+    }
+    else {
+      console.log('getRoomsAtBranch query SUCCESS\n')
+      response.send(rows);
+    }
+  });
 }
 
 
